36780c48d0af379977ac542eaaf7821469315d47
Samantha Wong <smiwong920@gmail.com>
2019-02-22 00:11:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 def speak_to_grandma(phrase)
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+  
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.downcase()
-    return "NO, NOT SINCE 1938!"
-  else 
-    return "HUH?! SPEAK UP, SONNY!"
   end
 end
